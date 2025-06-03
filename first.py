27fae58b9444360e0cefda7fe0cd9d2692c2db83
sumanbhattarai2008 <sumanbhattarai20080228@gmail.com>
2025-06-03 10:49:19 +0545

    Day 1/100

--- first.py
+++ first.py
@@ -1,20 +1,50 @@
-print("Day 1- Python PRint Function")
+#Missing days are just quiz and about the course writer.
+#Day 1-4
+print("Hello World!")
+
+#Day 1-5
+print("Day 1- Python Print Function")
 print("The function is declared like this:")
 print("print('what to print')")
 
+#Day 1-6
 print("HEllo!\nWord")
 print("Hello + Angela")
 print("Hello" + "Angela")
 print("Hello" + " Angela")
 print("Hello" + " " + "Angela")
+
+#Day 1-7
+#Brief Introduction about string manipulation and concatenation
+
+#Day 1-8
 print("Hello " +  input("What is your name?"))
+
+#Day 1-9
 print(len(input("What is your name?")))
-name= "JACK"
-city= "New York"
-pet= "dog"
-name = "JACK"
-print((name)  + " " + ( pet))
-a="thomas"
+a = "Angelica"
 print(len(a))
 print(a[0:3])
 print(len("anonymous"))
+
+#Day 1-10
+#Python Variables
+name = "Angela"
+print(name)
+
+#Day 1-11
+a = input("a: ")
+b = input("b: ")
+sum = a + b #Use int before input to get sum.
+print(sum)
+a = b
+b = a
+
+#Day 1-12
+#Naming Variables
+
+#Day 1-14
+print("Welcome to the band name generator!")
+city = input("Which city did you grew up in?: ")
+pet = input("What is the name of a pet?: ")
+print("Your band name could be " + city + " " + pet )
--- third.py
+++ third.py
@@ -1,14 +1,30 @@
-print("Enter to the roolercoaster")
+print("Enter to the rollercoaster")
 height = int(input("What is your height in cm?"))
 if height > 120:
- print("You can ride the roolercoaster!")
+ print("You can ride the rollercoaster!")
 else:
  print("You can not ride the rollercoaster.")
- n = int(input("Enter a number: "))
- if n % 2 == 0:
+age= int(input("What is your age? ")) 
+if age < 18:
+ bill = 5
+ print("Child tickets are $5")
+elif age <= 18:
+ bill = 7
+ print("Youth tickets are $7")
+elif age>70 and age<80:
+ print("Everything is free and your bill is $0")
+else:
+  bill = 12
+  print("Adult tickets are $12") 
+photo = input("Do you want a photo or not? Y or N: ")
+if photo == "Y":
+  bill += 3
+print(f"Your total bill is ${bill}")
+n = int(input("Enter a number: "))
+if n % 2 == 0:
   print("This is an even number.")
- else:
-  print("This is an odd number.")
+else:
+ print("This is an odd number.")
 salary = int(input("Enter your salary: "))
 if salary > 100000:
  tax = salary * 0.3
@@ -31,3 +47,26 @@ elif 30 <= bmi < 35:
  print(f"Your BMI is {bmi} and you are overweight.")
 else:
  print(f"Your BMI is {bmi} and you are clinically obese.")
+
+ size = input("What size pizza do you want?: S, M, L: ")
+add_pepperoni = input("Do you want pepperoni? Y or N: ")
+pepperoni_size = input("What size of pepperoni do you want?: S or M: ")
+extra_cheese = input("Do you want extra cheese? Y or N: ")
+bill = 0
+
+if size == "S":
+ bill += 15
+elif size == "M":
+ bill += 20
+else:
+ bill += 25
+  
+if add_pepperoni == "Y":
+ if pepperoni_size == "S":
+    bill += 2
+ elif pepperoni_size == "M":
+    bill += 3
+
+if extra_cheese == "Y":
+  bill += 1
+print(f"Yor final bill is: ${bill}")
