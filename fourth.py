0861e001d16995f9328999fbe95dcea3d88a3067
sumanbhattarai2008 <sumanbhattarai20080228@gmail.com>
2025-06-04 13:22:59 +0545

    Python Detailed Learning from 0

--- fourth.py
+++ fourth.py
@@ -1,4 +1,59 @@
 import random
+random_integer = random.randint(1, 999)
+print(random_integer)
+random_float = random.random() * 5
+print(random_float)
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}%.")
+
+
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+random_side = random.randint(0, 1)
+if random_side == 0:
+    print("Heads")
+else:
+    print("Tails")    
+
+
+countries = ["Nepal" , "India" , "China" , "Bangladesh" , "Bhutan" , "America", ] 
+print(countries[4])
+print(countries[-1])  
+countries.extend(["Switzerland", "Germany"])
+print(countries)
+countries[1] = "Sweden"
+print(countries)
+
+
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+name = input("Give me everybody's name seperated by a comma: ")
+names = name.split(",")
+randome_name = random.choice(names)
+print(f"{randome_name} is going to buy the meal today!")
+
+fruits = ["Apple", "Mango", "Banana", "Litchi"]  
+vegetables = ["Potato", "Tomato", "Onion", "Carrot"]  
+List = fruits + vegetables
+print(List)
+dirty_dozen = [fruits, vegetables]
+print(dirty_dozen)
+
+row1 = ["☐" , "☐" , "☐"]
+row2 = ["☐" , "☐" , "☐"]
+row3 = ["☐" , "☐" , "*"]
+map = [row1 , row2 , row3]
+print(f"{row1}\n{row2}\n{row3}\n")
+position = input("Where do you want to put the treasure? ")
+horizontal = int(position[0])
+vertical = int(position[1])
+selected_row = map[vertical - 1]
+selected_row[horizontal - 1] = "X"
+print(f"{row1}\n{row2}\n{row3}\n")
+
+
+
+import random
 a = player_choice = int(input("What is your choice> 0 for rock, 1 for paper, 2 for scissors?: "))
 b = computer_choice = random.randint(0, 2)
 print(f"Computer choice: {computer_choice}")
