1873e78f30f888b3551b5d0419724131318cb9b0
sumanbhattarai2008 <sumanbhattarai20080228@gmail.com>
2025-06-05 14:30:12 +0545

    Day 5/100

--- Python/fifth.py
+++ Python/fifth.py
@@ -70,8 +70,10 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 password = ""
 for char in range(1, nr_letters + 1):
    password += random.choice(letters)
+
 for char in range(1, nr_symbols + 1):
    password += random.choice(symbols)
+   
 for char in range(1, nr_numbers + 1):
     password += random.choice(numbers)
 print (password)
