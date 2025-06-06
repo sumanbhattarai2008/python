80181d1fe8e0794d3dd65dbd3d9f6aa0b11a6938
sumanbhattarai2008 <sumanbhattarai20080228@gmail.com>
2025-06-06 08:55:00 +0545

    Day 5/100

--- Python/fifth.py
+++ Python/fifth.py
@@ -35,16 +35,6 @@ for number in range(1,101):
         total2 += number
 print(total2)
 
-total3 = 0
-for number in range(1,101):
-    if number % 2 == 3:
-        print("Fizz ")
-    if number % 5 == 0:
-        print("Buzz ")
-    if number % 3 == 0 and number % 5 == 0:
-        print("FizzBuzz ")
-    else:
-        print(number)
 
 total3 = 0
 for number in range(1,101):
@@ -61,8 +51,7 @@ for number in range(1,101):
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+', '-']
 print("Welcome to the PyPassword Generator!")
 nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
@@ -73,7 +62,7 @@ for char in range(1, nr_letters + 1):
 
 for char in range(1, nr_symbols + 1):
    password += random.choice(symbols)
-   
+
 for char in range(1, nr_numbers + 1):
     password += random.choice(numbers)
 print (password)
